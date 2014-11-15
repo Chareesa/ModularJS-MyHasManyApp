789a76cf00b960e1ac5f8d0a9f35f550362eecc7
Chareesa <chareesagraham@gmail.com>
2014-11-15 12:35:54 -0800

    added comments

--- routes/actor_routes.js
+++ routes/actor_routes.js
@@ -4,6 +4,7 @@ var Actor = require('../models/actor.js');
 
 module.exports = function(app) {
 
+  //creates an actor
   app.post('/api/actors', function(req, res) {
     var actor = new Actor(req.body);
     actor.save(function(err, data) {
@@ -12,6 +13,7 @@ module.exports = function(app) {
     });
   });
 
+  //updates an actor
   app.put('/api/actors/:actorId', function(req, res) {
     var actor = req.body;
     delete actor._id;
@@ -21,6 +23,7 @@ module.exports = function(app) {
     });
   });
 
+  //displays all actors
   app.get('/api/actors', function(req, res) {
     Actor.find({}, function(err, data) {
       if (err) return res.status(500).send('there was an error');
@@ -28,6 +31,7 @@ module.exports = function(app) {
     });
   });
 
+  //displays an actor
   app.get('/api/actors/:actorId', function(req, res) {
     Actor.findOne({_id: req.params.actorId}, function(err, data) {
       if (err) return res.status(500).send('there was an error');
@@ -35,6 +39,7 @@ module.exports = function(app) {
     });
   });
 
+  //removes an actor
   app.delete('/api/actors/:actorId', function(req, res) {
     Actor.remove({_id: req.params.actorId}, function(err) {
       if (err) return res.status(500).send('there was an error');
--- routes/movie_routes.js
+++ routes/movie_routes.js
@@ -4,6 +4,7 @@ var Movie = require('../models/movie.js');
 
 module.exports = function(app) {
 
+  //creates a movie
   app.post('/api/movies', function(req, res) {
     var movie = new Movie(req.body);
     movie.save(function(err, data) {
@@ -12,6 +13,7 @@ module.exports = function(app) {
     });
   });
 
+  //updates a movie
   app.put('/api/movies/:movieId', function(req, res) {
     var movie = req.body;
     delete movie._id;
@@ -21,6 +23,7 @@ module.exports = function(app) {
     });
   });
 
+  //displays all movies created
   app.get('/api/movies', function(req, res) {
     Movie.find({}, function(err, data) {
       if (err) return res.status(500).send('there was an error');
@@ -28,6 +31,7 @@ module.exports = function(app) {
     });
   });
 
+  //displays a movie
   app.get('/api/movies/:movieId', function(req, res) {
     Movie.findOne({_id: req.params.movieId}, function(err, data) {
       if (err) return res.status(500).send('there was an error');
@@ -35,6 +39,7 @@ module.exports = function(app) {
     });
   });
 
+  //deletes a movie
   app.delete('/api/notes/:movieId', function(req, res) {
     Movie.remove({_id: req.params.movieId}, function(err) {
       if (err) return res.status(500).send('there was an error');
